46bcc1402a820f22d750b42fe59820868bcce506
Didi Raggio <diego_raggio@hotmail.com>
2015-06-13 23:33:48 -0300

    second commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+  	@content_first = "This is a string of text";
+  	@content_second = "Yet another string of text";
+  	
+  end
+
+  def new
+  	
+  end
+
+  def create
+  	
+  end
+
+  def edit
+  	
+  end
+
+  def update
+  	
+  end
+
+  def show
+  	
+  end
+
+  def destroy
+  	
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'posts/index'
+  
   get 'home/index'
+
+    resources :posts
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
